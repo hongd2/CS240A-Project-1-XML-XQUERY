bd6b55f831c5ff87bd5290278f8f840b4e79dec4
mqchau <quanqchau@gmail.com>
2015-11-21 20:41:49 -0800

    could read query file and execute it

--- run_query.cpp
+++ run_query.cpp
@@ -6,41 +6,67 @@
 #include <iostream>
 #include <xqilla/xqilla-simple.hpp>
 #include <xqilla/runtime/Sequence.hpp>
+#include <string>
+#include <fstream>
+#include <streambuf>
+
+using namespace std;
 
 int main(int argc, char *argv[]) {
-  // Initialise Xerces-C and XQilla by creating the factory object
-  XQilla xqilla;
 
-  // Parse an XQuery expression
-  // (AutoDelete deletes the object at the end of the scope)
-  AutoDelete<XQQuery> query(xqilla.parse(X("/employees/employee")));
+    // check command line arguments
+    if (argc < 2){
+        cout << "Need name of query file" << endl;
+        return 1;
+    }
 
-  // Create a context object
-  AutoDelete<DynamicContext> context(query->createDynamicContext());
+    // read query string from file to std::string
+    std::ifstream t(argv[1]);
+    std::string query_str;
 
-  // Parse a document, and set it as the context item
-  Sequence seq = context->resolveDocument(X("v-depts.xml"));
-  if(!seq.isEmpty() && seq.first()->isNode()) {
+    t.seekg(0, std::ios::end);   
+    query_str.reserve(t.tellg());
+    t.seekg(0, std::ios::beg);
+
+    query_str.assign((std::istreambuf_iterator<char>(t)),
+                std::istreambuf_iterator<char>());
+
+    cout << "Query = " << query_str << endl;
+    
+    // Initialise Xerces-C and XQilla by creating the factory object
+    XQilla xqilla;
+
+    // Parse an XQuery expression
+    // (AutoDelete deletes the object at the end of the scope)
+    // AutoDelete<XQQuery> query(xqilla.parse(X("/employees/employee")));
+    AutoDelete<XQQuery> query(xqilla.parse(X(query_str.c_str())));
+
+    // Create a context object
+    AutoDelete<DynamicContext> context(query->createDynamicContext());
+
+    // Parse a document, and set it as the context item
+    Sequence seq = context->resolveDocument(X("v-depts.xml"));
+    if(!seq.isEmpty() && seq.first()->isNode()) {
     context->setContextItem(seq.first());
     context->setContextPosition(1);
     context->setContextSize(1);
-  }
+    }
 
-  Sequence seq2 = context->resolveDocument(X("v-emps.xml"));
-  if(!seq2.isEmpty() && seq2.first()->isNode()) {
+    Sequence seq2 = context->resolveDocument(X("v-emps.xml"));
+    if(!seq2.isEmpty() && seq2.first()->isNode()) {
     context->setContextItem(seq2.first());
     context->setContextPosition(2);
     context->setContextSize(1);
-  }
+    }
 
-  // Execute the query, using the context
-  Result result = query->execute(context);
+    // Execute the query, using the context
+    Result result = query->execute(context);
 
-  // Iterate over the results, printing them
-  Item::Ptr item;
-  while(item = result->next(context)) {
-    std::cout << UTF8(item->asString(context)) << std::endl;
-  }
+    // Iterate over the results, printing them
+    Item::Ptr item;
+    while(item = result->next(context)) {
+        std::cout << UTF8(item->asString(context)) << std::endl;
+    }
 
-  return 0;
+    return 0;
 }
